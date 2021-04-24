b7172032767a47d212ebf9ec596d1d69d7c589f4
Camilo Agudelo <jcagudelo12@gmail.com>
2021-04-24 12:36:07 -0500

    Update ListTasks.jsx

--- components/tasks/ListTasks.jsx
+++ components/tasks/ListTasks.jsx
@@ -1,12 +1,60 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Divider } from "react-native-elements";
 
-export default function ListTasks() {
+export default function ListTasks({ tasks, navigation }) {
   return (
     <View>
-      <Text></Text>
+      <FlatList
+        data={tasks}
+        keyExtractor={(index) => index.toString()}
+        onEndReachedThreshold={0.5}
+        renderItem={(task) => <Task task={task} />}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const Task = ({ task }) => {
+  const { id, name, user } = task.item;
+
+  const goTask = () => {
+    console.log("Presioné la tarea: ", name);
+  };
+  return (
+    <TouchableOpacity onPress={goTask}>
+      <View style={styles.viewTask}>
+        <View style={styles.viewTaskInformation}>
+          <Text style={styles.taskTitle}>{name}</Text>
+        </View>
+      </View>
+      <Divider style={styles.divider} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewTask: {
+    flexDirection: "row",
+    margin: 10,
+  },
+  viewTaskInformation: {
+    width: "100%",
+    paddingRight: 4,
+  },
+  taskTitle: {
+    textAlignVertical: "center",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  divider: {
+    backgroundColor: "#442484",
+    margin: 10,
+  },
+});
